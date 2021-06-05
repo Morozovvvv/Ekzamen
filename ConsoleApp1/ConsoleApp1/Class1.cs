21e31d3baef87abfed18aa597d8659e2f823c44f
Morozovvvv <77523805+Morozovvvv@users.noreply.github.com>
2021-06-05 15:06:27 +0400

    Ekzamen

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        public int sum = 0;
+        public int Sum
+        {
+            set
+            {
+                sum += value;
+            }
+            get
+            {
+                return sum;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int a=0;
+           Class1 clas = new Class1();
+           while (clas.Sum<100)
+            {
+                Console.Write(" ");
+                clas.Sum=int.Parse(Console.ReadLine());
+                a++;
+            }
+            Console.WriteLine("Сумма чисел = {0}",clas.Sum);
+            Console.WriteLine("количество просуммированных чисел = {0}",a);
+            Console.ReadKey();
+        }
+    }
+}
